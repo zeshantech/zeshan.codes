0b9b0e35ec8bcd4618b518c1e40ea8777320a65d
zeshantech <zeshanshakil0@gmail.com>
2024-09-06 13:30:17 +0500

    feat: updated tech

--- src/app/home/tech-stack/tech-stack.component.ts
+++ src/app/home/tech-stack/tech-stack.component.ts
@@ -7,19 +7,19 @@ import { Component } from '@angular/core';
 })
 export class TechStackComponent {
   backendTechnologies = [
-    { name: "JavaScript", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710757611/qcrj2jqbqdp0uduabrkd.png' },
-    { name: "Python", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710758304/fehvkcaqverghy5o0iij.png' },
-    { name: "Golang", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710758146/qlamaxt3fc1wdzr2tj2f.png' },
-    { name: "TypeScript", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009761/otj7i99gcvkj6moeok54.png' },
-    { name: "Nodejs", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009760/f05i1vkplc2j3ft5hxoj.png' },
-    { name: "Expressjs", imageUrl: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png' },
+    // { name: "JavaScript", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710757611/qcrj2jqbqdp0uduabrkd.png' },
+    // { name: "Golang", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710758146/qlamaxt3fc1wdzr2tj2f.png' },
+    // { name: "TypeScript", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009761/otj7i99gcvkj6moeok54.png' },
+
     { name: "Nestjs", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009756/ph8a6wv0nrrps778fcco.png' },
+    { name: "Nodejs", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009760/f05i1vkplc2j3ft5hxoj.png' },
+    { name: "GinGonic", imageUrl: 'https://avatars.githubusercontent.com/u/15729372?s=280&v=4' },
+    { name: "Expressjs", imageUrl: 'https://cdn.icon-icons.com/icons2/2699/PNG/512/expressjs_logo_icon_169185.png' },
     { name: "Moleculerjs", imageUrl: 'https://moleculer-java.github.io/site/logo.png' },
-    { name: "FastAPI", imageUrl: 'https://www.jetbrains.com/guide/assets/fastapi-6837327b.svg' },
     { name: "Microservices", imageUrl: 'https://media.licdn.com/dms/image/C5112AQEEcR2mak_D8Q/article-cover_image-shrink_720_1280/0/1563209644780?e=2147483647&v=beta&t=U76cYNVN1rJWoYx1fKFsMW9-3DbfyImlwWGFhLLc3qk' },
+    { name: "E2E Testing", imageUrl: 'https://cdn-icons-png.flaticon.com/512/10435/10435145.png' },
     { name: "Graphql", imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/GraphQL_Logo.svg/1200px-GraphQL_Logo.svg.png' },
     { name: "Firebase", imageUrl: 'https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png' },
-    { name: "Numpy", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710018441/jhwrbhifybvnbszvt62t.png' },
   ];
   
   frontendTechnologies = [
@@ -28,26 +28,25 @@ export class TechStackComponent {
     { name: "Reactjs", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009761/hnux1zmysqxclff9rac1.png' },
     { name: "Nextjs", imageUrl: 'https://static-00.iconduck.com/assets.00/nextjs-icon-512x512-y563b8iq.png' },
     { name: "Angular", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009752/j0meorzfnkyk995pfgzr.png' },
-    { name: "Tauri", imageUrl: 'https://raw.githubusercontent.com/tauri-apps/tauri/HEAD/app-icon.png' },
   ]
   
   databases = [
     { name: "PostgresQl", imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png' },
     { name: "MySql", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009755/xvwcprf2eohpc98dwjro.png' },
-    { name: "SQlite", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009761/lzfxckgyhkqlfrloar3i.png' },
     { name: "MariaDB", imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-mariadb-226022.png?f=webp&w=256' },
+    { name: "SQlite", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009761/lzfxckgyhkqlfrloar3i.png' },
     { name: "MongoDB", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009753/clo5kbdsb1mlfczslpn7.png' },
     { name: "Redis", imageUrl: 'https://www.stackery.io/assets/images/posts/redis-cache-cluster-support/featured.svg' },
     { name: "DynamoDB", imageUrl: 'https://cdn2.iconfinder.com/data/icons/amazon-aws-stencils/100/Database_copy_DynamoDB-512.png'},
-    { name: "Firestore", imageUrl: 'https://seeklogo.com/images/F/firestore-logo-3828671CC5-seeklogo.com.png'}
   ]
   
   devopsTechnologies = [
     { name: "AWS", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009750/wm0h2lbbzxogenttx0tg.png' },
-    { name: "Azure", imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Microsoft_Azure.svg/1200px-Microsoft_Azure.svg.png' },
     { name: "Docker", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009749/pebtirzmkqmputt8o0pf.png' },
     { name: "Git", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009750/dmcnjjohqxrujnm27gy1.png' },
-    { name: "Github", imageUrl: 'https://res.cloudinary.com/djvfnekle/image/upload/v1710009751/drztbwozq90q6iwnxwu8.png' },
-    { name: "Genkins", imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1200px-Jenkins_logo.svg.png' },
+    { name: "Gitlens", imageUrl: 'https://static-00.iconduck.com/assets.00/githubactions-icon-2048x2048-ipqow27x.png' },
+    { name: "Jenkins", imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Jenkins_logo.svg/1200px-Jenkins_logo.svg.png' },
+    { name: "Ansible", imageUrl: 'https://cdn.iconscout.com/icon/free/png-256/free-asible-logo-icon-download-in-svg-png-gif-file-formats--brand-pack-logos-icons-9631116.png' },
+    { name: "Linux", imageUrl: 'https://static-00.iconduck.com/assets.00/linux-icon-2048x2048-sy06t4un.png' },
   ]
 }
